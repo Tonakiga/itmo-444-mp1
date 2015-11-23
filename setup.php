2304dd46cb04f68aac8828ae1d0ce78c5dceb7c4
tonakiga <j.sebastien.seck@gmail.com>
2015-11-22 18:20:01 -0600

    Fixed the database creation! Hurraysudo git add *!

--- setup.php
+++ setup.php
@@ -1,68 +1,80 @@
 <?php
+ini_set('display_errors',1); 
+ error_reporting(E_ALL);
 // Start the session^M
 require '/var/www/html/vendor/autoload.php';
 $rds = new Aws\Rds\RdsClient([
     'version' => 'latest',
-    'region'  => 'us-east-1'
+    'region'  => 'us-east-1',
+    'credentials' => [
+        'key'    => 'AKIAI37TXF3MPNO7NLWQ',
+        'secret' => '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',
+    ],
 ]);
 $result = $rds->createDBInstance([
-    'AllocatedStorage' => 10,
+    'AllocatedStorage' =>10,
     #'AutoMinorVersionUpgrade' => true || false,
     #'AvailabilityZone' => '<string>',
     #'BackupRetentionPeriod' => <integer>,
-	#'CharacterSetName' => '<string>',
-	#'CopyTagsToSnapshot' => true || false,
-	#'DBClusterIdentifier' => '<string>',
+   # 'CharacterSetName' => '<string>',
+   # 'CopyTagsToSnapshot' => true || false,
+   # 'DBClusterIdentifier' => '<string>',
     'DBInstanceClass' => 'db.t1.micro', // REQUIRED
-    'DBInstanceIdentifier' => 'jss-itmo444-mp1', // REQUIRED
-    'DBName' => 'jss_itmo444_mp1',
+    'DBInstanceIdentifier' => 'jss-test-db', // REQUIRED
+    'DBName' => 'jsstestdb',
     #'DBParameterGroupName' => '<string>',
     #'DBSecurityGroups' => ['<string>', ...],
     #'DBSubnetGroupName' => '<string>',
     'Engine' => 'MySQL', // REQUIRED
-    'EngineVersion' => '5.5.41',
+   # 'EngineVersion' => '5.5.41',
     #'Iops' => <integer>,
     #'KmsKeyId' => '<string>',
-	#'LicenseModel' => '<string>',
-	'MasterUserPassword' => 'letmein1234',
+   # 'LicenseModel' => '<string>',
+  'MasterUserPassword' => 'letmein1234',
     'MasterUsername' => 'controller',
     #'MultiAZ' => true || false,
     #'OptionGroupName' => '<string>',
     #'Port' => <integer>,
     #'PreferredBackupWindow' => '<string>',
     #'PreferredMaintenanceWindow' => '<string>',
-    'PubliclyAccessible' => true,
+ 'PubliclyAccessible' => true,
     #'StorageEncrypted' => true || false,
     #'StorageType' => '<string>',
-	#'Tags' => [
-	#     [
-	#         'Key' => '<string>',
-	#         'Value' => '<string>',
-	#     ],
+   # 'Tags' => [
+   #     [
+   #         'Key' => '<string>',
+   #         'Value' => '<string>',
+   #     ],
         // ...
-	# ],
+   # ],
     #'TdeCredentialArn' => '<string>',
     #'TdeCredentialPassword' => '<string>',
-	#6'VpcSecurityGroupIds' => ['<string>', ...],
+   # 'VpcSecurityGroupIds' => ['<string>', ...],
 ]);
 print "Create RDS DB results: \n";
 # print_r($rds);
-$result = $rds->waitUntil('DBInstanceAvailable',['DBInstanceIdentifier' => 'jss_itmo444_mp1',
+$result = $rds->waitUntil('DBInstanceAvailable',['DBInstanceIdentifier' => 'jss-test-db',
 ]);
 // Create a table 
 $result = $rds->describeDBInstances([
-    'DBInstanceIdentifier' => 'jss_itmo444_mp1',
+    'DBInstanceIdentifier' => 'jss-test-db',
 ]);
 $endpoint = $result['DBInstances'][0]['Endpoint']['Address'];
 print "============\n". $endpoint . "================\n";
-$link = mysqli_connect($endpoint,"controller","letmein1234","3306") or die("Error " . mysqli_error($link)); 
-echo "Here is the result: " . $link;
-$sql = "CREATE TABLE images
+$link = mysqli_connect($endpoint,"controller","letmein1234","jsstestdb") or die("Error " . mysqli_error($link));
+#echo "Here is the result: " . $link;
+$sql = "CREATE TABLE IF NOT EXISTS items  
 (
-ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-PosterName VARCHAR(32),
-Title VARCHAR(32),
-Content VARCHAR(500)
+    id INT NOT NULL AUTO_INCREMENT,
+    uname VARCHAR(20) NOT NULL,
+    email VARCHAR(20) NOT NULL,
+    phone VARCHAR(20) NOT NULL,
+    s3rawurl VARCHAR(256) NOT NULL,
+    s3finishedurl VARCHAR(256) NOT NULL,
+    jpgfilename VARCHAR(255) NOT NULL,
+    state TINYINT(3) NOT NULL,
+    datecolumn TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
+    PRIMARY KEY(id)
 )";
-$con->query($sql);
+$link->query($sql);
 ?>
--- submit.php
+++ submit.php
@@ -1,53 +1,103 @@
 <?php
-
-echo "begin database";
-$link = mysqli_connect("itmo544jrhdb.cpyht2c1c9a4.us-east-1.rds.amazonaws.com","controller","letmein1234","jss_itmo444_mp1") or die("Error " . mysqli_error($link));
-
-/* check connection */
-if (mysqli_connect_errno()) {
-    printf("Connect failed: %s\n", mysqli_connect_error());
-    exit();
+session_start();
+var_dump($_POST);
+if(!empty($_POST)){
+echo $_POST['useremail'];
+echo $_POST['telephone'];
+echo $_POST['username'];
+$_SESSION['username']=$_POST['username'];
+$_SESSION['telephone']=$_POST['telephone'];
+$_SESSION['useremail']=$_POST['useremail'];
 }
-
-
-   # id INT NOT NULL AUTO_INCREMENT,
-   # name VARCHAR(200) NOT NULL,
-   # age INT NOT NULL,
-
-/* Prepared statement, stage 1: prepare */
-if (!($stmt = $link->prepare("INSERT INTO images (id, name, age) VALUES (NULL,?,?)"))) {
-    echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+else
+{
+echo "post empty";
 }
-
-$id = 1;
-$name = "joe";
-$age = 55;
-
-$stmt->bind_param("si",$name,$age);
-
+date_default_timezone_set('America/Chicago');
+$uploaddir = '/tmp/';
+$uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
+print '<pre>';
+if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
+  echo "File is valid, and was successfully uploaded.\n";
+}
+else {
+    echo "Possible file upload attack!\n";
+}
+echo 'Here is some more debugging info:';
+print_r($_FILES);
+print "</pre>";
+  
+require '/var/www/html/vendor/autoload.php';
+$s3 = new Aws\S3\S3Client([
+    'version' => 'latest',
+    'region'  => 'us-east-1'
+]);
+$bucket = uniqid("jss-php",false);
+# AWS PHP SDK version 3 create bucket
+$result = $s3->createBucket([
+    'ACL' => 'public-read',
+    'Bucket' => $bucket
+]);
+$s3->waitUntil('BucketExists',[
+	'Bucket' => $bucket
+]);
+$result = $s3->putObject([
+    'ACL' => 'public-read',
+    'Bucket' => $bucket,
+   'Key' => $uploadfile,
+   'ContentType' => $FILES['userfile']['type'],
+   'Body' =>fopen($uploadfile)
+]);
+$url = $result['ObjectURL'];
+echo $url;
+$rds = new Aws\Rds\RdsClient([
+    'version' => 'latest',
+    'region'  => 'us-east-1'
+]);
+$result = $rds->describeDBInstances(array(
+    'DBInstanceIdentifier' => 'jss-db'
+   
+));
+$endpoint = $result['DBInstances'][0]['Endpoint']['Address'];
+    echo "============\n". $endpoint . "================";
+$link = mysqli_connect($endpoint,"controller","letmein1234","jssdb") or die("Error" . mysql_error($link));
+if (mysqli_connect_errno()) { 
+    printf("Connect failed: %s\n", mysqli_connect_error()); 
+    exit(); 
+} 
+else { 
+echo "Success"; 
+} 
+if (!($stmt = $link->prepare("INSERT INTO userImages (username,useremail,telephone,raws3url,finisheds3url,filename,state) VALUES (?,?,?,?,?,?,?)"))) {
+    echo "Prepare failed: (" . $link->errno . ") " . $link->error;
+}
+$username=$_POST['username'];
+$useremail = $_POST['useremail'];
+$telephone = $_POST['telephone'];
+$raws3url = $url; 
+$filename = basename($_FILES['userfile']['name']);
+$finisheds3url = "none";
+$state=0;
+$stmt->bind_param("ssssssi",$username,$useremail,$telephone,$raws3url,$finisheds3url,$filename,$state);
 if (!$stmt->execute()) {
     echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
 }
-
 printf("%d Row inserted.\n", $stmt->affected_rows);
-
-
-/* explicit close recommended */
 $stmt->close();
-
-$link->real_query("SELECT * FROM images");
+$publish = $result->publish(array(
+    'TopicArn' => $topicARN,
+    // Message is required
+    'Subject' => 'Test',
+    'Message' => 'Image Uploaded',
+    
+    
+));
+$link->real_query("SELECT * FROM userImages");
 $res = $link->use_result();
-
 echo "Result set order...\n";
 while ($row = $res->fetch_assoc()) {
-    echo " id = " . $row['id'] . "\n";
+    echo $row['id'] . " " . $row['username'] . " " . $row['useremail']. " " . $row['telephone'];
 }
-
-
 $link->close();
-
-
-
-
-?>
-
+header("Location: gallery.php");
+?> 
