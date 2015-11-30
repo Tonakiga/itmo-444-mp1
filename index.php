355765dad88d1003ccb13492f59ba0aa913e8a5d
tonakiga <j.sebastien.seck@gmail.com>
2015-11-29 23:40:04 -0600

    Hopefully fixed index.php

--- index.php
+++ index.php
@@ -3,11 +3,16 @@
 <head><title>JSS File Page</title>
 </head>
 <body>
-Jean Sebastien Seck File Uploader:
+<h1>Jean Sebastien Seck File Uploader:</h1>
 <!-- Button for DB creation -->
 <form enctype="multipart/form-data" action="setup.php" method="POST">
-Click to create DB: <br />
+<h2>Click to create DB: </h2><br />
 <input type="submit" value="Create Database" />
+</form>
+<hr />
+<form enctype="multipart/form-data" action="createTable.php" method="POST">
+<h2>Click to create table in DB: </h2><br />
+<input type="submit" value="Create Table in DB" />
 </form>
 <hr />
 <!-- The data encoding type (enctype) MUST be specified -->
@@ -16,16 +21,16 @@ Click to create DB: <br />
 <!-- MAX_FILE_SIZE must precede the file input field -->
 <input type="hidden" name="MAX_FILE_SIZE" value="3000000" />
 <!-- User file name sets name in $_FILES array -->
-File to Upload:<br /> 
-<input name="userfile" type="file" /><br />
-User Name: <br />
-<input type = "uname" name = "username"><br />
-User Email: <br />
-<input type="email" name="useremail"><br />
-User Phone Number: <br />
-Ex: [1-800-867-5309]: <br />
-<input type="phone" name="phone"><br />
-Click to Upload File: <br />
+<h2>File to Upload:</h2><br /> 
+<input type="file" name="userFileForm" /><br />
+<h3><i>User Name:</i></h3><br />
+<input type = "uname" name = "userNameForm"><br />
+<h3><i>User Email:</i></h3><br />
+<input type="email" name="userEmailForm"><br />
+<h3><i>User Phone Number:</i></h3><br />
+<h3><i>Ex: [1-800-867-5309]:</i></h3><br />
+<input type="phone" name="userTelephoneForm"><br />
+<h3><i>Click to Upload File:</i></h3><br />
 <input type="submit" value="Send File" />
 </form>
 <!-- Adds a line to seperate gallery browsing from upload form -->
@@ -34,7 +39,7 @@ Click to Upload File: <br />
 <form enctype="multipart/form-data" action="gallery.php" method="POST">
     
 Enter User Email to Browse Gallery: <br />
-<input type="email" name="email">
+<input type="email" name="userInputEmail">
 <input type="submit" value="Load Gallery" />
 </form>
 </body>
